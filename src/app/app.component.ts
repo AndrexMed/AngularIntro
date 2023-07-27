d08962dc4e5a3171a1f391d6769385ad70f43b45
AndrexMed <giovanniandrex@gmail.com>
2023-07-26 23:27:11 -0500

    FirstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularIntro';
+  nombre = "Andres"
+  edad = 25
+  img = "https://source.unsplash.com/random"
+
+  btnDisabled = true
+
+  Persona = {
+    name: "El Gio",
+    age: 25,
+    avatar: "https://source.unsplash.com/random"
+  }
+
+  Register = {
+    name: "xxx",
+    email: "",
+    pass: ""
+  }
+
+  Test = "Hello World"
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled
+  }
+  incrementarEdad() {
+    this.Persona.age += 1
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement
+  }
+
+  changeH2(event: Event) {
+    const element = event.target as HTMLInputElement
+    this.Test = element.value
+  }
+
+  registrar() {
+    console.log(this.Register)
+  }
+
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,13 @@
+const username: string | number = "Giovanni"
+
+const suma = (a: number, b: number) => a + b
+
+suma(1,1)
+
+class Persona{
+
+    constructor(private Nombre: string, public edad: number){
+    }
+}
+
+const Andres = new Persona("ElGio", 25)
