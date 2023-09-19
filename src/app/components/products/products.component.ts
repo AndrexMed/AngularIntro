b9210f081ce09442283dfa8d11124c6093526cab
AndrexMed <giovanniandrex@gmail.com>
2023-09-18 23:35:34 -0500

    catchError - bugs

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { retry } from 'rxjs';
 import { Product, CreateProductDTO, UpdateProductDTO } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
 import { StoreService } from 'src/app/services/store.service';
@@ -31,21 +32,29 @@ export class ProductsComponent {
     description: ''
   };
 
+  limit = 10
+  offset = 0
+
+  statusDetail: 'loading' | 'success' | 'error' | 'init' = 'init';
+
   constructor(private storeService: StoreService,
     private productService: ProductService) {
     this.myShoppingCartProducts = this.storeService.GetMyShoppingCartProducts()
   }
 
   ngOnInit(): void {
-    this.productService.GetAllProducts().subscribe(
-      response => {
-        console.log(response)
-        this.products = response
-      },
-      error => {
-        console.log(error)
-      }
-    )
+    // this.productService.GetAllProducts().subscribe(
+    // this.productService.getsProductsByPage(10, 0).subscribe(
+    //   response => {
+    //     console.log(response)
+    //     this.products = response
+    //     this.offset += this.limit
+    //   },
+    //   error => {
+    //     console.log(error)
+    //   }
+    // )
+    this.loadMore()
   }
 
   onAddToShoppingCart(product: Product) {
@@ -58,14 +67,23 @@ export class ProductsComponent {
   }
 
   onShowDetail(id: string) {
-    console.log(id)
-    this.productService.GetProduct(id).subscribe(
-      data => {
-        console.log(data)
-        this.toggleProductDetail();
-        this.productChosen = data;
-      }
-    )
+    this.statusDetail = 'loading';
+    this.toggleProductDetail();
+    // console.log(id)
+    this.productService.GetProduct(id)
+      .subscribe(
+        data => {
+          // console.log(data)
+          // this.toggleProductDetail();
+          this.productChosen = data;
+          this.statusDetail = 'success';
+        },
+        error => {
+          console.log(error)
+          window.alert(error)
+          this.statusDetail = 'error';
+        }
+      )
   }
 
   createNewProduct() {
@@ -82,17 +100,41 @@ export class ProductsComponent {
     })
   }
 
-  updateProduct(){
+  updateProduct() {
     const changes: UpdateProductDTO = {
       title: "Nuevo Titulo",
     }
     const id = this.productChosen.id
     this.productService.update(id, changes)
-    .subscribe(data => {
-      console.log("Update: ", data)
-      const productIndex = this.products.findIndex(item => item.id === this.productChosen.id)
-      this.products[productIndex] = data
-    })
+      .subscribe(data => {
+        console.log("Update: ", data)
+        const productIndex = this.products.findIndex(item => item.id === this.productChosen.id)
+        this.products[productIndex] = data
+      })
+  }
+
+  deleteProduct() {
+    const id = this.productChosen.id
+    this.productService.delete(id).subscribe(
+      () => {
+        const productIndex = this.products.findIndex(item => item.id === this.productChosen.id)
+        this.products.splice(productIndex, 1)
+        this.showProductDetail = false
+      }
+    )
+  }
+
+  loadMore() {
+    return this.productService.getsProductsByPage(this.limit, this.offset)
+      .pipe(retry(3))
+      .subscribe(
+        data => {
+          this.products = this.products.concat(data)
+          this.offset += this.limit
+        }, error => {
+          console.log(error)
+        }
+      )
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,31 +1,67 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
+import { catchError, retry } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.prod';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  private APIURL = `${environment.API_URL}`;
+
   private apiUrl = "https://fakestoreapi.com/products"
   private apiUrl2 = "https://api.escuelajs.co/api/v1/products"
 
+
   constructor(private http: HttpClient) { }
 
-  GetAllProducts(){
-    return this.http.get<Product[]>(this.apiUrl2)
+  GetAllProducts(limit?: string, offset?: string) {
+
+    let params = new HttpParams()
+
+    if (limit && offset) {
+      params = params.set("limit", limit)
+      params = params.set("offset", offset)
+    }
+    return this.http.get<Product[]>(this.APIURL, {params} )
+    // .pipe(retry(3)) no funciono aqui
   }
 
-  GetProduct(id: string){
-    return this.http.get<Product>(`${this.apiUrl2}/${id}`)
+  GetProduct(id: string) {
+    return this.http.get<Product>(`${this.APIURL}/${id}`)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === HttpStatusCode.Conflict) {
+          return throwError('Algo esta fallando en el server');
+        }
+        if (error.status === HttpStatusCode.NotFound) {
+          return throwError('El producto no existe');
+        }
+        if (error.status === HttpStatusCode.Unauthorized) {
+          return throwError('No estas permitido');
+        }
+        return throwError('Ups algo salio mal');
+      })
+    )
   }
 
-  create(product: CreateProductDTO){
-    return this.http.post<Product>(this.apiUrl2, product)
+  create(product: CreateProductDTO) {
+    return this.http.post<Product>(this.APIURL, product)
   }
 
-  update(id: string, dto: UpdateProductDTO){
-    return this.http.put<Product>(`${this.apiUrl2}/${id}`, dto)
+  update(id: string, dto: UpdateProductDTO) {
+    return this.http.put<Product>(`${this.APIURL}/${id}`, dto)
+  }
+
+  delete(id: string) {
+    return this.http.delete<boolean>(`${this.APIURL}/${id}`)
+  }
+
+  getsProductsByPage(limit: number, offset: number) {
+    return this.http.get<Product[]>(this.APIURL, { params: { limit, offset } })
   }
 
 }
